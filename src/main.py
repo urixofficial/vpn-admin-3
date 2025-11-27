4de3f137928280accf4e962543064321c7ca1734
urixofficial <urixofficial@gmail.com>
2025-11-27 12:04:17 +1000

    All telegram functions moved to src/telegram

--- src/main.py
+++ src/main.py
@@ -1,25 +1,14 @@
 import asyncio
 
-from aiogram import Dispatcher, Bot
-
-from src.core.config import settings
-from src.core.logger import log
-
-from telegram.handlers import router as main_router
-# from src.transaction.user_control import router as transaction_router
-
+from core.config import settings
+from core.logger import log
+from telegram.bot import run_bot
 
 
 async def main():
 	log.info("Запуск {} {}".format(settings.APP_NAME, settings.APP_VERSION))
 
-	dp = Dispatcher()
-	dp.include_router(main_router)
-	# dp.include_router(transaction_router)
-	# dp.include_router(core_router)
-
-	bot = Bot(settings.TELEGRAM_TOKEN)
-	await dp.start_polling(bot)
+	await run_bot()
 
 
 if __name__ == "__main__":
---
+++ src/telegram/bot.py
@@ -0,0 +1,14 @@
+from aiogram import Dispatcher, Bot
+from telegram.handlers import router as main_router
+
+from core.config import settings
+from core.logger import log
+
+async def run_bot():
+	log.debug("Запуск бота...")
+
+	dp = Dispatcher()
+	dp.include_router(main_router)
+
+	bot = Bot(settings.TELEGRAM_TOKEN)
+	await dp.start_polling(bot)
