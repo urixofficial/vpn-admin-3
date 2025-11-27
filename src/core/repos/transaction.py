5e03e53fd01ade815cceb5754a5ca05178b5df8f
urixofficial <urixofficial@gmail.com>
2025-11-27 22:02:30 +1000

    Get transaction by user id added to transaction repository

--- src/core/repos/transaction.py
+++ src/core/repos/transaction.py
@@ -1,3 +1,6 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from core.models.transaction import TransactionModel
 from core.schemas.transaction import (
 	CreateTransaction,
@@ -5,11 +8,20 @@ from core.schemas.transaction import (
 	UpdateTransaction,
 )
 
+from core.logger import log
+from core.database import connection
+
 from .base import BaseRepo
 
 
 class TransactionRepo(BaseRepo[CreateTransaction, ReadTransaction, UpdateTransaction, TransactionModel]):
-	pass
+	@connection
+	async def get_by_user(self, user_id: int, session: AsyncSession) -> list[ReadTransaction]:
+		log.debug("Получение списка транзакций по пользователю")
+		query = select(TransactionModel).where(TransactionModel.user_id == user_id).order_by(TransactionModel.id.desc())
+		transaction_models = await session.execute(query)
+		await session.commit()
+		return [ReadTransaction.model_validate(transaction_model) for transaction_model in transaction_models]
 
 
 transaction_repo = TransactionRepo(CreateTransaction, ReadTransaction, UpdateTransaction, TransactionModel)
