5a4a4e1e981bffe64be0f16da5e1b0ebafb4ca9d
urixofficial <urixofficial@gmail.com>
2025-12-02 10:16:36 +1000

    User telegram router added

--- src/telegram/handlers/__init__.py
+++ src/telegram/handlers/__init__.py
@@ -1,8 +1,10 @@
 from aiogram import Router
 
 from .admin import router as admin_router
+from .user import router as user_router
 
 router = Router(name="main_router")
 router.include_router(admin_router)
+router.include_router(user_router)
 
 __all__ = {"router"}
---
+++ src/telegram/handlers/user/__init__.py
@@ -0,0 +1,5 @@
+from aiogram import Router
+
+router = Router(name="user_router")
+
+__all__ = {"router"}
