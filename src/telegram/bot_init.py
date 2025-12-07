387439e4b9cc9281760dc66f40c91573c0847ae8
urixofficial <urixofficial@gmail.com>
2025-12-08 08:28:35 +1000

    HTML parse mode set by default

--- src/telegram/bot_init.py
+++ src/telegram/bot_init.py
@@ -1,5 +1,7 @@
 from aiogram import Bot
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 
 from core.config import settings
 
-bot = Bot(settings.tg.token)
+bot = Bot(settings.tg.token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
